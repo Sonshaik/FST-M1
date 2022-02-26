9c1cd9ba32696354dfad44bb13da2c630ab5ac10
Sonshaik <97940311+Sonshaik@users.noreply.github.com>
2022-02-26 17:14:19 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity4.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity4 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.training-support.net");
+        System.out.println("page title is : "+driver.getTitle());
+        driver.findElement(By.xpath("/html/body/div/div/div/a")).click();
+        System.out.println("page title is : "+driver.getTitle());
+
+        driver.quit();
+    }}
---
+++ Cucumber/Activities/package-info.java
@@ -0,0 +1,12 @@
+package Features;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
